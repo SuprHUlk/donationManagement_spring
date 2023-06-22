c76f07f7d0e704a8f1ae3a819db5d1c53da4d84d
Ayush Kumar <119495476+SuprHUlk@users.noreply.github.com>
2023-06-22 23:39:38 +0530

    Delete src directory

--- src/main/java/com/project/donationmanagement/repository/DonorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.donationmanagement.repository;
-
-import com.project.donationmanagement.entity.Donor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DonorRepository extends JpaRepository<Donor, String> {
-}
